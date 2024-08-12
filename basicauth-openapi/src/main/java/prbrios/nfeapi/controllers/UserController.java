b821a5af800630bce2f3509d9ed86043d7fb6c98
Paulo Rios <prbrios@gmail.com>
2024-08-12 15:40:37 -0300

    Configurado o requerimento de seguranca no controller

--- basicauth-openapi/src/main/java/prbrios/nfeapi/controllers/UserController.java
+++ basicauth-openapi/src/main/java/prbrios/nfeapi/controllers/UserController.java
@@ -1,5 +1,6 @@
 package prbrios.nfeapi.controllers;
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -13,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/user")
+@SecurityRequirement(name = "basicAuth")
 public class UserController {
 
     @Autowired
